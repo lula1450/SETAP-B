9abcfe726c4b2563cd9f21cbc3e39c0fdf0e5ea9
l-howieson <lhowieson14@gmail.com>
2026-02-13 14:08:10 +0000

    more progress on the schedule.py file, created a database.py file to allow interaction between APIs and our database

--- petsync_backend/routers/schedule.py
+++ petsync_backend/routers/schedule.py
@@ -4,9 +4,35 @@
 # Imports required for request routing, database interaction, and handling
 # time-based scheduling data within the Scheduling & Reminders Service.
 from fastapi import APIRouter, HTTPException
-from sqlalchemy.orm import session
-from datetime import datetime
+from sqlalchemy.orm import Session
+from datetime import datetime, date, time
 
 #imports from the models.py file, these are database tables represented in python
 from models import PetAppointment, FeedingSchedule, Reminder
 
+router = APIRouter(
+    prefix = "/schedule",
+    tags = ["Scheduling & Reminders"]
+)
+
+#Function to create pet appointment 
+
+@router.post("/Appointments")
+def create_pet_appointment(
+    pet_id: int,
+    appointment_date: date,
+    appointment_time: time,
+    db: Session = Depends(get_db)
+):
+    appointment = PetAppointment(
+        pet_id=pet_id,
+        pet_appointment_date=appointment_date,
+        pet_appointment_time=appointment_time,
+        appointment_status="SCHEDULED"
+    )
+
+    db.add(appointment)
+    db.commit()
+    db.refresh(appointment)
+
+    return appointment
