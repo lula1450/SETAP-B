a5cdeba438985da9e70202288a509633e8c9767e
lulaaaa <up2266457@myport.ac.uk>
2026-02-14 17:28:56 +0000

    trigger 4

--- SQL backend/triggers.sql
+++ SQL backend/triggers.sql
@@ -1,3 +1,4 @@
+---TRIGGER1---
 ---Set appointment to 'completed' when updated after its time or date.---
 CREATE OR REPLACE FUNCTION auto_complete_appointment()
 RETURNS TRIGGER AS $$
@@ -19,6 +20,7 @@ BEFORE UPDATE ON pet_appointment
 FOR EACH ROW
 EXECUTE FUNCTION auto_complete_appointment();
 
+---TRIGGER2---
 ---After a reminder is sent it is updated to sent---
 CREATE OR REPLACE FUNCTION auto_update_reminder_status()
 RETURNS TRIGGER AS $$
@@ -39,6 +41,7 @@ BEFORE UPDATE ON reminder
 FOR EACH ROW
 EXECUTE FUNCTION auto_update_reminder_status();
 
+---TRIGGER3---
 ---When reminider is updated and set to sent it creates the same feeding schedule same time for the next day---
 CREATE OR REPLACE FUNCTION create_next_feeding_reminder()
 RETURNS TRIGGER AS $$
@@ -86,7 +89,49 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+---TRIGGER4---
+---Creates appointmnet reminders daily or weekly---
+CREATE OR REPLACE FUNCTION create_initial_appointment_reminder()
+RETURNS TRIGGER AS $$
+DECLARE
+    first_date DATE;
+BEGIN
+    -- If reminders disabled or frequency = none, do nothing
+    IF NEW.enable_reminder = FALSE OR NEW.reminder_frequency = 'none' THEN
+        RETURN NEW;
+    END IF;
 
+    -- Determine first reminder date
+    IF NEW.reminder_frequency = 'daily' THEN
+        first_date := NEW.pet_appointment_date - INTERVAL '1 day';
+
+    ELSIF NEW.reminder_frequency = 'weekly' THEN
+        first_date := NEW.pet_appointment_date - INTERVAL '7 days';
+
+    ELSE
+        -- once
+        first_date := NEW.pet_appointment_date;
+    END IF;
+
+    -- Create the first reminder
+    INSERT INTO reminder (
+        pet_appointment_id,
+        feeding_schedule_id,
+        reminder_date,
+        reminder_time,
+        reminder_status
+    )
+    VALUES (
+        NEW.pet_appointment_id,
+        NULL,
+        first_date,
+        NEW.pet_appointment_time,
+        'Pending'
+    );
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
 
 
