67a646845372a93cbde6b403985d824644737b02
Lauren <laurennrcoppin@gmail.com>
2026-03-04 14:25:35 +0000

    created new virtual environment

--- petsync_backend/venv/bin/activate.fish
+++ petsync_backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/workspaces/SETAP-B/petsync_backend/venv"
+set -gx VIRTUAL_ENV "/Users/laurencoppin/Documents/Year 2/SETAP/SETAP-B/petsync_backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
