f04d48865eb99851ff6e3ddb1cf0b68ee4fa0c2f
Lauren <laurennrcoppin@gmail.com>
2026-04-07 13:41:25 +0100

    removed unnecessary comments from main

--- petsync_backend/main.py
+++ petsync_backend/main.py
@@ -2,27 +2,11 @@
 from petsync_backend.routers import auth, pets, health, schedule, reports, owners
 from petsync_backend.middleware import PetSyncFirewall
 
-# allows the api gateway to handle concurrent requests
 from fastapi import FastAPI, HTTPException, Request
-
-# ensures that the data entering the user manager or pet manger is typed and validated
 from pydantic import BaseModel
-
 from fastapi.middleware.cors import CORSMiddleware
-
-# process historical health data stored in the database
-# make comparisons between data entries
 import pandas as pd
-
-# validatae that appointments are not set in the past - allows timestamps - chronological order
 from datetime import datetime
-
-# create the FastAPI app instance
-
-# pip install - uvicorn - environment to host api - ensures the application layer remains stable
-
-# background dependency
-# pip install - python-multipart - allows FastAPI to handle form data and file uploads - HRM from AUTH UI to OSS
 
 
 app = FastAPI(
@@ -30,10 +14,8 @@ app = FastAPI(
     version="1.0.0"
 )
 
-# Register the Security Firewall FIRST - before CORS
 app.add_middleware(PetSyncFirewall)
 
-# Existing CORS configuration
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
